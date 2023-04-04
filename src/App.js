f72b214ac9b60753bf1583415f5d2ab69ce9d863
Rama <cahyaramdhn12@gmail.com>
2023-04-05 01:06:54 +0700

    upload movie-database

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+import './css/style.css'
+import Home from "./pages/Home";
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
